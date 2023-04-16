57d1ce42a05aea51ed9d73f22732d8527098d149
tauantcamargo <lukyhenson@gmail.com>
2023-04-16 00:24:33 -0300

    Initial Commit

---
+++ SwiftUiBullseye/SwiftUiBullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiBullseyeApp.swift
+//  SwiftUiBullseye
+//
+//  Created by Tauan Camargo on 16/04/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiBullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
