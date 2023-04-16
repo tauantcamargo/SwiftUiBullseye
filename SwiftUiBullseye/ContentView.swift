e86d14fc23473254a8393c1da7447f07117dada9
tauantcamargo <lukyhenson@gmail.com>
2023-04-16 01:27:47 -0300

    feat: adding views to bullseye project

--- SwiftUiBullseye/ContentView.swift
+++ SwiftUiBullseye/ContentView.swift
@@ -8,19 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+  @State private var alertIsVisible: Bool = false
+  @State private var sliderValue: Double = 50.0
+  
+  var body: some View {
+    VStack {
+      Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+        .bold()
+        .multilineTextAlignment(.center)
+        .lineSpacing(4.0)
+        .font(.footnote)
+        .kerning(2.0)
+      Text("89")
+        .kerning(-1.0)
+        .font(.largeTitle)
+        .fontWeight(.black)
+      HStack {
+        Text("1")
+          .bold()
+          .padding()
+        Slider(value: $sliderValue, in: 1.0...100.0)
+        Text("100")
+          .bold()
+          .padding()
+      }
+      Button {
+        alertIsVisible.toggle()
+      } label: {
+        Text("Hit me")
+      }
+      .alert("Hello There!", isPresented: $alertIsVisible, actions: {
+        Button {
+        } label: {
+          Text("Hit me")
         }
-        .padding()
+      }, message: {
+        var roundedValue: Int = Int(sliderValue.rounded())
+        Text("The slider's value is \(roundedValue)")
+      })
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
